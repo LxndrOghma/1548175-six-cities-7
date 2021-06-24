031b5521b41887dd99a4a794a0c4421100193166
LxndrOghma <Brikman424@gmail.com>
2021-06-24 20:27:25 +0300

    Добавит описание действий и reducer

---
+++ project/src/store/reducer.js
@@ -0,0 +1,26 @@
+import { offers } from '../mocks/offers';
+import { ActionType } from './action';
+
+const initialState = {
+  city: 'Amsterdam',
+  offers: offers,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.CHANGE_CITY:
+      return {
+        ...state,
+        city: action.payload,
+      };
+    case ActionType.FILL_OFFERS_LIST:
+      return {
+        ...state,
+        offers: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export { reducer };
