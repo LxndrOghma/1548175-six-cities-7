690356624c11daba58b48047e3ebf15b3fdeb091
LxndrOghma <Brikman424@gmail.com>
2021-06-24 20:34:50 +0300

    Подключит хранилище к приложению, добавит поддержку Redux DevTools

--- project/src/index.js
+++ project/src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools} from 'redux-devtools-extension';
+
 import App from './components/app/app';
 import {offers, nearestOffers} from './mocks/offers';
 import reviews from './mocks/reviews';
+import { reducer } from './store/reducer';
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      offers={offers}
-      reviews={reviews}
-      nearestOffers={nearestOffers}
-    />
+    <Provider store={store}>
+      <App
+        offers={offers}
+        reviews={reviews}
+        nearestOffers={nearestOffers}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
