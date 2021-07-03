cee2f58ca084a5bacb336ec3a3cb77c7dcf9b28e
LxndrOghma <Brikman424@gmail.com>
2021-07-03 18:26:07 +0300

    Подключит history в App

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router as BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import MainPage from '../pages/main-page/main-page';
 import SignInPage from '../pages/sign-in-page/sign-in-page';
@@ -8,11 +8,12 @@ import RoomPage from '../pages/room-page/room-page';
 import NotFoundPage from '../pages/not-found-page/not-found-page';
 import { AppRoute } from '../../const';
 import PrivateRoute from '../private-route/private-route';
+import browserHistory from '../../browser-history';
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter history={browserHistory}>
       <Switch>
         <Route exact path={AppRoute.MAIN}>
           <MainPage/>
