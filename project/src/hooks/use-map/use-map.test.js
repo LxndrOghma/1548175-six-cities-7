a11e0160912d002796fe6904a0826ef82498a3d1
LxndrOghma <Brikman424@gmail.com>
2021-07-21 20:59:08 +0300

    Добавит тест для хука useMap

---
+++ project/src/hooks/use-map/use-map.test.js
@@ -0,0 +1,41 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useMap from './use-map';
+
+const mockLocation = {
+  latitude: 48.85661,
+  longitude: 2.351499,
+  zoom: 10,
+};
+
+const mapContainer = {
+  current: '<section className="map"/>',
+};
+
+jest.mock('leaflet', () => ({
+  __esModule: true,
+  default: {
+    map() {
+      return 'mock map';
+    },
+    tileLayer() {
+      return this;
+    },
+    addTo() {
+      return this;
+    },
+    icon() {
+      return 'mock icon';
+    },
+  },
+}));
+
+describe('Hook: useMap', () => {
+
+  it('should return map', () => {
+    const {result} = renderHook(() =>
+      useMap(mapContainer, mockLocation),
+    );
+
+    expect(result.current).toBe('mock map');
+  });
+});
