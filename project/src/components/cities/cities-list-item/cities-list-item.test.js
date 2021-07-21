b03fdc83f36ff90da59be338ffbbeeec949ce095
LxndrOghma <Brikman424@gmail.com>
2021-07-21 22:15:20 +0300

    Добавит тесты для компонентов CitiesList и CitiesListItem

--- project/src/components/cities/cities-list-item/cities-list-item.test.js
+++ project/src/components/cities/cities-list-item/cities-list-item.test.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
 
 import CitiesListItem from './cities-list-item';
 
 describe('Component: CitiesListItem', () => {
   it('should render correctly', () => {
     const history = createMemoryHistory();
-    const {getByText} = render(
-      <Router history={history} >
-        <CitiesListItem city={'Paris'} onCityChange={() => {}} activeCity={'Paris'} />
+    const onCityChange = jest.fn();
+
+    const {container} = render(
+      <Router history={history}>
+        <CitiesListItem city={'Paris'} onCityChange={onCityChange} activeCity={'Paris'} />
       </Router>,
     );
-    const cityElement = getByText('Paris');
 
-    expect(cityElement).toBeInTheDocument();
+    expect(screen.getByText('Paris')).toBeInTheDocument();
+    expect(container.querySelector('.tabs__item--active')).toBeInTheDocument();
+    userEvent.click(screen.getByText('Paris'));
+    expect(onCityChange).toBeCalled();
   });
 });
