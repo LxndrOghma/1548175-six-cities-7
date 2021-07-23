abeabdc56b058d33ddd2b7daacdcd727d439326d
LxndrOghma <Brikman424@gmail.com>
2021-07-24 00:40:22 +0300

    Исправит предупреждение в Map

--- project/src/components/map/map.test.js
+++ project/src/components/map/map.test.js
@@ -74,40 +74,7 @@ const mockOffers = [
 
 ];
 
-const mockActiveCard = {
-  bedrooms: 2,
-  city: {
-    location: {
-      latitude: 52.370216,
-      longitude: 4.895168,
-      zoom: 10,
-    },
-    name: 'Paris',
-  },
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-  goods: ['Heating', 'Kitchen', 'Washing machine', 'Coffee machine'],
-  host: {
-    avatarUrl: 'img/test.jpg',
-    id: 11,
-    isPro: false,
-    name: 'Horus',
-  },
-  id: 33,
-  images: ['img/test.jpg, img/test2.jpg'],
-  isFavorite: true,
-  isPremium: true,
-  location: {
-    latitude: 52.369553943508,
-    longitude: 4.85309666406198,
-    zoom: 8,
-  },
-  maxAdults: 2,
-  previewImage: 'img/test.jpg',
-  price: 150,
-  rating: 3,
-  title: 'Test offer 3',
-  type: 'hotel',
-};
+const mockActiveCard = 3;
 
 describe('Component: Map', () => {
 
